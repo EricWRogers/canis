360bd4930a4385945c5dfc2b2386cfc4085a5bdc
EricWRogers <ericwilliamrogers@gmail.com>
2021-12-20 08:06:59 -0600

    Hello Triangle

--- src/Game.cpp
+++ src/Game.cpp
@@ -8,21 +8,86 @@ void Game::Run() {
 
     frame_counter = 0;
 
-    Canis::Window window;
+    window.Spawn("Canis Engine Demo", 1280, 720, 0);
 
     Load();
-
-    window.Spawn("Canis Engine Demo", 1280, 720, 0);
 
     target_fixed_update_ticks = (1.0f / 16.0f) * 1000;
     last_fixed_update = SDL_GetTicks();  
 
-    frame_limiter.Init(8000.0f);
+    frame_limiter.Init(60.0f);
 
     Loop();
 }
 
-void Game::Load() {}
+void Game::Load() {
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // left  
+         0.5f, -0.5f, 0.0f, // right 
+         0.0f,  0.5f, 0.0f  // top   
+    }; 
+
+    //unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+}
 
 void Game::Loop() {
     while(_game_state == GameState::PLAY) {
@@ -39,6 +104,9 @@ void Game::Loop() {
         InputUpdate();
         Update();
         Draw();
+
+        window.SwapBuffer();
+
         LateUpdate();
 
         fps = frame_limiter.End();
@@ -52,13 +120,26 @@ void Game::Loop() {
     }
 }
 
-void Game::Update() {}
+void Game::Update() {
+
+}
 
 void Game::Draw() {
     Canis::Log("Draw");
+
+    glClearColor(0.2f,0.3,0.3f,1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // draw first triangle
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(0);
 }
 
-void Game::LateUpdate() {}
+void Game::LateUpdate() {
+
+}
 
 void Game::FixedUpdate(float _delta_time) {
     Canis::Log("FixedUpdate: " + std::to_string(_delta_time));
--- src/Game.hpp
+++ src/Game.hpp
@@ -35,6 +35,8 @@ private:
 
     Canis::Limiter frame_limiter;
 
+    Canis::Window window;
+
     int frame_counter;
     
     float fps;
@@ -42,4 +44,23 @@ private:
     float last_fixed_update;
     float current_ticks_since_last_fixed_update;
     float target_fixed_update_ticks;
+
+    // move out to external class
+    unsigned int vertexShader;
+    unsigned int shaderProgram;
+    unsigned int VBO, VAO;
+
+    const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+    const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
 };
