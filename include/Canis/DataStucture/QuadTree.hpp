a18e5317bcc6a45fb99aad78789c92ad86846b8e
EricWRogers <ericwilliamrogers@gmail.com>
2022-11-14 21:22:24 -0600

    using index instead of pointer

--- include/Canis/DataStucture/QuadTree.hpp
+++ include/Canis/DataStucture/QuadTree.hpp
@@ -9,157 +9,195 @@ namespace Canis
 struct QuadPoint {
     glm::vec2 position;
     entt::entity entity;
-
-    /*QuadPoint(glm::vec2 _position, entt::entity _entity) {
-        position = _position;
-        entity = _entity;
-    }*/
 };
 
 struct QuadNode
 {
-    QuadNode *top0 = nullptr;
-    QuadNode *top1 = nullptr;
-    QuadNode *bottom0 = nullptr;
-    QuadNode *bottom1 = nullptr;
+    unsigned short top0 = 0;
+    unsigned short top1 = 0;
+    unsigned short bottom0 = 0;
+    unsigned short bottom1 = 0;
     glm::vec2 center = glm::vec2(0.0f);
     float size;
     QuadPoint points[20] = {};
     int pointsCapacity = 20;
     int numOfPoints = 0;
-
-    QuadNode() {
-
-    }
-
-    ~QuadNode() {
-        delete top0;
-        delete top1;
-        delete bottom0;
-        delete bottom1;
-    }
 };
 
 class QuadTree
 {
 private:
-    QuadNode *root;
+    std::vector<QuadNode> quadNodes = {};
+    QuadNode quadNodeDefault = {};
+    unsigned int rootIndex = 0;
     unsigned int totalPoints = 0;
+    unsigned int totalNodes = 0;
+    unsigned int currentNumberOfNodes = 0;
 
-    void PointsQuery(const glm::vec2 &center, float radius, QuadNode *node, std::vector<QuadPoint> &results) {
-        if (node->top0 == nullptr) {
-            for(int i = 0; i < node->numOfPoints; i++) {
-                results.push_back(node->points[i]);
+    void PointsQuery(const glm::vec2 &center, float radius, unsigned short nodeIndex, std::vector<QuadPoint> &results) {
+        if (quadNodes[nodeIndex].top0 == 0) {
+            for(int i = 0; i < quadNodes[nodeIndex].numOfPoints; i++) {
+                results.push_back(quadNodes[nodeIndex].points[i]);
             }
             return;
         }
-        if (QuadtreeOverlap(center, radius, node->top0)) {
-            PointsQuery(center, radius, node->top0, results);
+        if (QuadtreeOverlap(center, radius, quadNodes[nodeIndex].top0)) {
+            PointsQuery(center, radius, quadNodes[nodeIndex].top0, results);
         }
-        if (QuadtreeOverlap(center, radius, node->top1)) {
-            PointsQuery(center, radius, node->top1, results);
+        if (QuadtreeOverlap(center, radius, quadNodes[nodeIndex].top1)) {
+            PointsQuery(center, radius, quadNodes[nodeIndex].top1, results);
         }
-        if (QuadtreeOverlap(center, radius, node->bottom0)) {
-            PointsQuery(center, radius, node->bottom0, results);
+        if (QuadtreeOverlap(center, radius, quadNodes[nodeIndex].bottom0)) {
+            PointsQuery(center, radius, quadNodes[nodeIndex].bottom0, results);
         }
-        if (QuadtreeOverlap(center, radius, node->bottom1)) {
-            PointsQuery(center, radius, node->bottom1, results);
+        if (QuadtreeOverlap(center, radius, quadNodes[nodeIndex].bottom1)) {
+            PointsQuery(center, radius, quadNodes[nodeIndex].bottom1, results);
         }
     }
 
-    bool QuadtreeOverlap(const glm::vec2 &point, const float &radius, QuadNode *node) {        
-        if (glm::distance( point, node->center) < radius + (node->size*1.4f))
+    bool QuadtreeOverlap(const glm::vec2 &point, const float &radius, unsigned short nodeIndex) {        
+        if (glm::distance( point, quadNodes[nodeIndex].center) < radius + (quadNodes[nodeIndex].size*1.4f))
             return true;
         
         return false;
     }
 
-    bool OctNodeBoundsContains(const glm::vec2 &point, QuadNode *node) {
-        float size = node->size/2.0f;
-        if (point.x > node->center.x - size && point.x < node->center.x + size &&
-            point.y > node->center.y - size && point.y < node->center.y + size)
+    bool OctNodeBoundsContains(const glm::vec2 &point, unsigned short nodeIndex) {
+        float size = quadNodes[nodeIndex].size/2.0f;
+        if (point.x > quadNodes[nodeIndex].center.x - size && point.x < quadNodes[nodeIndex].center.x + size &&
+            point.y > quadNodes[nodeIndex].center.y - size && point.y < quadNodes[nodeIndex].center.y + size)
             return true;
         
         return false;
     }
 
-    void AddPoint(const glm::vec2 &point, const entt::entity &entity, QuadNode *node) {
-        if (node->top0 == nullptr) {
-            if (node->pointsCapacity > node->numOfPoints) {
-                node->points[node->numOfPoints].position = point;
-                node->points[node->numOfPoints].entity = entity;
-                node->numOfPoints++;
+    void AddPoint(const glm::vec2 &point, const entt::entity &entity, unsigned short nodeIndex) {
+        if (quadNodes[nodeIndex].top0 == 0) {
+            if (quadNodes[nodeIndex].pointsCapacity > quadNodes[nodeIndex].numOfPoints) {
+                quadNodes[nodeIndex].points[quadNodes[nodeIndex].numOfPoints].position = point;
+                quadNodes[nodeIndex].points[quadNodes[nodeIndex].numOfPoints].entity = entity;
+                quadNodes[nodeIndex].numOfPoints++;
                 totalPoints++;
                 return;
             }
             
             // create octnodes
-            float size = node->size/2.0f;
+            float size = quadNodes[nodeIndex].size/2.0f;
             float halfSize = size/2.0f;
-            node->top0 = new QuadNode();
-            node->top0->size = size;
-            node->top0->center = node->center;
-            node->top0->center.x -= halfSize;
-            node->top0->center.y += halfSize;
 
-            node->top1 = new QuadNode();
-            node->top1->size = size;
-            node->top1->center = node->center;
-            node->top1->center.x += halfSize;
-            node->top1->center.y += halfSize;
+            currentNumberOfNodes++;
+            if (quadNodes.size() <= currentNumberOfNodes) {
+                quadNodes.push_back(quadNodeDefault);
+            }
+            quadNodes[nodeIndex].top0 = currentNumberOfNodes - 1;
+            quadNodes[quadNodes[nodeIndex].top0].size = size;
+            quadNodes[quadNodes[nodeIndex].top0].center = quadNodes[nodeIndex].center;
+            quadNodes[quadNodes[nodeIndex].top0].center.x -= halfSize;
+            quadNodes[quadNodes[nodeIndex].top0].center.y += halfSize;
+            quadNodes[quadNodes[nodeIndex].top0].top0 = 0;
+            quadNodes[quadNodes[nodeIndex].top0].top1 = 0;
+            quadNodes[quadNodes[nodeIndex].top0].bottom0 = 0;
+            quadNodes[quadNodes[nodeIndex].top0].bottom1 = 0;
+            quadNodes[quadNodes[nodeIndex].top0].numOfPoints = 0;
 
-            node->bottom0 = new QuadNode();
-            node->bottom0->size = size;
-            node->bottom0->center = node->center;
-            node->bottom0->center.x -= halfSize;
-            node->bottom0->center.y -= halfSize;
+            currentNumberOfNodes++;
+            if (quadNodes.size() <= currentNumberOfNodes) {
+                quadNodes.push_back(quadNodeDefault);
+            }
+            quadNodes[nodeIndex].top1 = currentNumberOfNodes - 1;
+            quadNodes[quadNodes[nodeIndex].top1].size = size;
+            quadNodes[quadNodes[nodeIndex].top1].center = quadNodes[nodeIndex].center;
+            quadNodes[quadNodes[nodeIndex].top1].center.x += halfSize;
+            quadNodes[quadNodes[nodeIndex].top1].center.y += halfSize;
+            quadNodes[quadNodes[nodeIndex].top1].top0 = 0;
+            quadNodes[quadNodes[nodeIndex].top1].top1 = 0;
+            quadNodes[quadNodes[nodeIndex].top1].bottom0 = 0;
+            quadNodes[quadNodes[nodeIndex].top1].bottom1 = 0;
+            quadNodes[quadNodes[nodeIndex].top1].numOfPoints = 0;
 
-            node->bottom1 = new QuadNode();
-            node->bottom1->size = size;
-            node->bottom1->center = node->center;
-            node->bottom1->center.x += halfSize;
-            node->bottom1->center.y -= halfSize;
+            currentNumberOfNodes++;
+            if (quadNodes.size() <= currentNumberOfNodes) {
+                quadNodes.push_back(quadNodeDefault);
+            }
+            quadNodes[nodeIndex].bottom0 = currentNumberOfNodes - 1;
+            quadNodes[quadNodes[nodeIndex].bottom0].size = size;
+            quadNodes[quadNodes[nodeIndex].bottom0].center = quadNodes[nodeIndex].center;
+            quadNodes[quadNodes[nodeIndex].bottom0].center.x -= halfSize;
+            quadNodes[quadNodes[nodeIndex].bottom0].center.y -= halfSize;
+            quadNodes[quadNodes[nodeIndex].bottom0].top0 = 0;
+            quadNodes[quadNodes[nodeIndex].bottom0].top1 = 0;
+            quadNodes[quadNodes[nodeIndex].bottom0].bottom0 = 0;
+            quadNodes[quadNodes[nodeIndex].bottom0].bottom1 = 0;
+            quadNodes[quadNodes[nodeIndex].bottom0].numOfPoints = 0;
+
+            currentNumberOfNodes++;
+            if (quadNodes.size() <= currentNumberOfNodes) {
+                quadNodes.push_back(quadNodeDefault);
+            }
+            quadNodes[nodeIndex].bottom1 = currentNumberOfNodes - 1;
+            quadNodes[quadNodes[nodeIndex].bottom1].size = size;
+            quadNodes[quadNodes[nodeIndex].bottom1].center = quadNodes[nodeIndex].center;
+            quadNodes[quadNodes[nodeIndex].bottom1].center.x += halfSize;
+            quadNodes[quadNodes[nodeIndex].bottom1].center.y -= halfSize;
+            quadNodes[quadNodes[nodeIndex].bottom1].top0 = 0;
+            quadNodes[quadNodes[nodeIndex].bottom1].top1 = 0;
+            quadNodes[quadNodes[nodeIndex].bottom1].bottom0 = 0;
+            quadNodes[quadNodes[nodeIndex].bottom1].bottom1 = 0;
+            quadNodes[quadNodes[nodeIndex].bottom1].numOfPoints = 0;
 
             // load points into child node
-            for(int i = 0; i < node->numOfPoints; i++) {
-                if (OctNodeBoundsContains(point, node->top0)) {
-                    AddPoint(point, entity, node->top0);
-                } else if (OctNodeBoundsContains(point, node->top1)) {
-                    AddPoint(point, entity, node->top1);
-                } else if (OctNodeBoundsContains(point, node->bottom0)) {
-                    AddPoint(point, entity, node->bottom0);
-                } else if (OctNodeBoundsContains(point, node->bottom1)) {
-                    AddPoint(point, entity, node->bottom1);
+            for(int i = 0; i < quadNodes[nodeIndex].numOfPoints; i++) {
+                if (OctNodeBoundsContains(point, quadNodes[nodeIndex].top0)) {
+                    AddPoint(point, entity, quadNodes[nodeIndex].top0);
+                } else if (OctNodeBoundsContains(point, quadNodes[nodeIndex].top1)) {
+                    AddPoint(point, entity, quadNodes[nodeIndex].top1);
+                } else if (OctNodeBoundsContains(point, quadNodes[nodeIndex].bottom0)) {
+                    AddPoint(point, entity, quadNodes[nodeIndex].bottom0);
+                } else if (OctNodeBoundsContains(point, quadNodes[nodeIndex].bottom1)) {
+                    AddPoint(point, entity, quadNodes[nodeIndex].bottom1);
                 }
             }
         }
 
-        if (OctNodeBoundsContains(point, node->top0)) {
-            AddPoint(point, entity, node->top0);
+        if (OctNodeBoundsContains(point, quadNodes[nodeIndex].top0)) {
+            AddPoint(point, entity, quadNodes[nodeIndex].top0);
             return;
-        } else if (OctNodeBoundsContains(point, node->top1)) {
-            AddPoint(point, entity, node->top1);
+        } else if (OctNodeBoundsContains(point, quadNodes[nodeIndex].top1)) {
+            AddPoint(point, entity, quadNodes[nodeIndex].top1);
             return;
-        } else if (OctNodeBoundsContains(point, node->bottom0)) {
-            AddPoint(point, entity, node->bottom0);
+        } else if (OctNodeBoundsContains(point, quadNodes[nodeIndex].bottom0)) {
+            AddPoint(point, entity, quadNodes[nodeIndex].bottom0);
             return;
-        } else if (OctNodeBoundsContains(point, node->bottom1)) {
-            AddPoint(point, entity, node->bottom1);
+        } else if (OctNodeBoundsContains(point, quadNodes[nodeIndex].bottom1)) {
+            AddPoint(point, entity, quadNodes[nodeIndex].bottom1);
             return;
         }
     }
 
 public:
     QuadTree(glm::vec2 center, float size) {
-        QuadNode *node = new QuadNode();
-        node->size = size;
-        node->center = center;
-
-        root = node;
+        rootIndex = 0;
+        totalNodes = 0;
+        quadNodes.push_back(quadNodeDefault);
+        quadNodes[rootIndex].size = size;
+        quadNodes[rootIndex].center = center;
     }
 
     ~QuadTree() {
-        delete root;
+    }
+
+    void Reset() {
+        currentNumberOfNodes = 1;
+        float size = quadNodes[rootIndex].size;
+        glm::vec2 center = quadNodes[rootIndex].center;
+        rootIndex = 0;
+        quadNodes[rootIndex].size = size;
+        quadNodes[rootIndex].center = center;
+        quadNodes[rootIndex].top0 = 0;
+        quadNodes[rootIndex].top1 = 0;
+        quadNodes[rootIndex].bottom0 = 0;
+        quadNodes[rootIndex].bottom1 = 0;
+        quadNodes[rootIndex].numOfPoints = 0;
     }
 
     void PrintTotalPoints() {
@@ -167,30 +205,29 @@ public:
     }
 
     void AddPoint(glm::vec2 point, entt::entity entity) {
-        AddPoint(point, entity, root);
+        AddPoint(point, entity, rootIndex);
     }
 
     bool PointsQuery(const glm::vec2 &center, float radius, std::vector<QuadPoint> &results) {
-        if (QuadtreeOverlap(center, radius, root)) {
-            //Canis::Log("true");
-            if (root->top0 == nullptr) {
-                for(int i = 0; i < root->numOfPoints; i++) {
-                    results.push_back(root->points[i]);
+        if (QuadtreeOverlap(center, radius, rootIndex)) {
+            if (quadNodes[rootIndex].top0 == 0) {
+                for(int i = 0; i < quadNodes[rootIndex].numOfPoints; i++) {
+                    results.push_back(quadNodes[rootIndex].points[i]);
                 }
             }
             else
             {
-                if (QuadtreeOverlap(center, radius, root->top0)) {
-                    PointsQuery(center, radius, root->top0, results);
+                if (QuadtreeOverlap(center, radius, quadNodes[rootIndex].top0)) {
+                    PointsQuery(center, radius, quadNodes[rootIndex].top0, results);
                 }
-                if (QuadtreeOverlap(center, radius, root->top1)) {
-                    PointsQuery(center, radius, root->top1, results);
+                if (QuadtreeOverlap(center, radius, quadNodes[rootIndex].top1)) {
+                    PointsQuery(center, radius, quadNodes[rootIndex].top1, results);
                 }
-                if (QuadtreeOverlap(center, radius, root->bottom0)) {
-                    PointsQuery(center, radius, root->bottom0, results);
+                if (QuadtreeOverlap(center, radius, quadNodes[rootIndex].bottom0)) {
+                    PointsQuery(center, radius, quadNodes[rootIndex].bottom0, results);
                 }
-                if (QuadtreeOverlap(center, radius, root->bottom1)) {
-                    PointsQuery(center, radius, root->bottom1, results);
+                if (QuadtreeOverlap(center, radius, quadNodes[rootIndex].bottom1)) {
+                    PointsQuery(center, radius, quadNodes[rootIndex].bottom1, results);
                 }
             }
             return true;
