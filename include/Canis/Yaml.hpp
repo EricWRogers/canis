56ec81cb8a45746f58adf5527402a91a622a1ee2
EricWRogers <ericwilliamrogers@gmail.com>
2023-01-25 22:40:30 -0600

    yaml convert for glm::vec

--- include/Canis/Yaml.hpp
+++ include/Canis/Yaml.hpp
@@ -3,23 +3,78 @@
 #include <yaml-cpp/yaml.h>
 
 namespace YAML {
-template <>
-struct convert<glm::vec2> {
-    inline static Node encode(const glm::vec2 &v) {
-        Node node;
-        node.push_back(v.x);
-        node.push_back(v.y);
-        node.SetStyle(EmitterStyle::Flow);
-        return node;
-    }
+template<>
+	struct convert<glm::vec2>
+	{
+		static Node encode(const glm::vec2& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.SetStyle(EmitterStyle::Flow);
+			return node;
+		}
 
-    inline static bool decode(const Node &node, glm::vec2 &v) {
-        if (!node.IsSequence() || node.size() != 2) {
-            return false;
-        }
-        v.x = node[0].as<float>();
-        v.y = node[1].as<float>();
-        return true;
-    }
-};
+		static bool decode(const Node& node, glm::vec2& rhs)
+		{
+			if (!node.IsSequence() || node.size() != 2)
+				return false;
+
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			return true;
+		}
+	};
+
+	template<>
+	struct convert<glm::vec3>
+	{
+		static Node encode(const glm::vec3& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.push_back(rhs.z);
+			node.SetStyle(EmitterStyle::Flow);
+			return node;
+		}
+
+		static bool decode(const Node& node, glm::vec3& rhs)
+		{
+			if (!node.IsSequence() || node.size() != 3)
+				return false;
+
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			rhs.z = node[2].as<float>();
+			return true;
+		}
+	};
+
+	template<>
+	struct convert<glm::vec4>
+	{
+		static Node encode(const glm::vec4& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.push_back(rhs.z);
+			node.push_back(rhs.w);
+			node.SetStyle(EmitterStyle::Flow);
+			return node;
+		}
+
+		static bool decode(const Node& node, glm::vec4& rhs)
+		{
+			if (!node.IsSequence() || node.size() != 4)
+				return false;
+
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			rhs.z = node[2].as<float>();
+			rhs.w = node[3].as<float>();
+			return true;
+		}
+	};
 } // end of YAML namespace
