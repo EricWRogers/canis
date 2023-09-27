a6a4dea578732cbb8fbab1dce6a14c6471ceec2e
EricWRogers <ericwilliamrogers@gmail.com>
2023-09-27 14:25:31 -0500

    Improve error message

--- src/IOManager.cpp
+++ src/IOManager.cpp
@@ -140,9 +140,7 @@ namespace Canis
 		FILE *file = fopen(path.c_str(), "r");
 		if (file == NULL)
 		{
-			printf("Impossible to open the file ! Are you in the right path ? See Tutorial 1 for details\n");
-			getchar();
-			return false;
+			FatalError("Can not open model: " + path);
 		}
 
 		while (1)
