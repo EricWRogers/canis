2b8926b24cee276cd55ca3cf27769db504bd0f2f
EricWRogers <ericwilliamrogers@gmail.com>
2023-11-15 10:44:29 -0600

    sort button to fix overlap problem

--- include/Canis/ECS/Systems/ButtonSystem.hpp
+++ include/Canis/ECS/Systems/ButtonSystem.hpp
@@ -1,17 +1,25 @@
 #pragma once
 #include <Canis/ECS/Systems/System.hpp>
+#include <Canis/External/entt.hpp>
 
 namespace Canis
 {
     class Scene;
 
+    struct ButtonAndDepth {
+        entt::entity entity;
+        float depth;
+    };
+
     class ButtonSystem : public System
     {
     private:
+        ButtonAndDepth* m_buttons = nullptr;
     public:
         ButtonSystem() : System() {}
+        ~ButtonSystem();
 
-        void Create() {}
+        void Create();
 
         void Ready() {}
 
--- src/ECS/Systems/ButtonSystem.cpp
+++ src/ECS/Systems/ButtonSystem.cpp
@@ -7,70 +7,96 @@
 #include <Canis/ECS/Components/Sprite2DComponent.hpp>
 #include <Canis/ECS/Components/ButtonComponent.hpp>
 #include <Canis/Math.hpp>
+#include <Canis/DataStructure/List.hpp>
 
 namespace Canis
 {
+    bool ButtonAndDepthAscending(void* a, void *b) {
+        return static_cast<ButtonAndDepth*>(a)->depth > static_cast<ButtonAndDepth*>(b)->depth;
+    }
+
+    ButtonSystem::~ButtonSystem()
+    {
+        if (m_buttons != nullptr)
+            List::Free(&m_buttons);
+    }
+
+    void ButtonSystem::Create()
+    {
+        if (m_buttons == nullptr)
+            List::Init(&m_buttons, 10, sizeof(ButtonAndDepth));
+    }
+
     void ButtonSystem::Update(entt::registry &_registry, float _deltaTime)
     {
         auto view = _registry.view<RectTransformComponent, ColorComponent, ButtonComponent>();
         glm::vec2 positionAnchor = glm::vec2(0.0f);
         glm::vec2 mouse = glm::vec2(0.0f);
 
+        List::Clear(&m_buttons);
+
         for (auto [entity, rect_transform, color, button] : view.each())
         {
             if (rect_transform.active)
             {
-                positionAnchor = GetAnchor((Canis::RectAnchor)rect_transform.anchor,
-                                           (float)window->GetScreenWidth(),
-                                           (float)window->GetScreenHeight());
-
-                mouse = GetInputManager().mouse;
-
-                if (rect_transform.rotation != 0.0f)
-                {
-                    float cAngle = cos(-rect_transform.rotation);
-                    float sAngle = sin(-rect_transform.rotation);
-
-                    glm::vec2 mouseOffset = mouse - (rect_transform.position + rect_transform.originOffset + positionAnchor);
-
-                    RotatePoint(mouseOffset, cAngle, sAngle);
-
-                    mouse = mouseOffset + (rect_transform.position + rect_transform.originOffset + positionAnchor);
-                }
-
-                if (mouse.x > rect_transform.position.x + positionAnchor.x + rect_transform.originOffset.x &&
-                    mouse.x < rect_transform.position.x + (rect_transform.size.x * rect_transform.scale) + rect_transform.originOffset.x + positionAnchor.x &&
-                    mouse.y > rect_transform.position.y + positionAnchor.y + rect_transform.originOffset.y &&
-                    mouse.y < rect_transform.position.y + (rect_transform.size.y * rect_transform.scale) + rect_transform.originOffset.y + positionAnchor.y)
-                {
-                    color.color = button.hoverColor;
-                    button.mouseOver = true;
-                    rect_transform.scale = button.hoverScale;
-
-                    if (button.action == 0u)
-                    {
-                        if (inputManager->JustLeftClicked())
-                        {
-                            button.func(button.instance);
-                            return;
-                        }
-                    }
-                    else if (button.action == 1u)
-                    {
-                        if (inputManager->LeftClickReleased())
-                        {
-                            button.func(button.instance);
-                            return;
-                        }
-                    }                    
-                }
-                else
-                {
-                    color.color = button.baseColor;
-                    button.mouseOver = false;
-                    rect_transform.scale = button.scale;
-                }
+                List::Add(&m_buttons, &entity);
+                color.color = button.baseColor;
+                button.mouseOver = false;
+                rect_transform.scale = button.scale;
             }
+        }
+
+        List::BubbleSort(&m_buttons, ButtonAndDepthAscending);
+
+        for (int i = 0; i < List::GetCount(&m_buttons); i++)
+        {
+            RectTransformComponent& rect_transform = _registry.get<RectTransformComponent>(m_buttons[i].entity);
+            ColorComponent& color = _registry.get<ColorComponent>(m_buttons[i].entity);
+            ButtonComponent& button = _registry.get<ButtonComponent>(m_buttons[i].entity);
+
+            positionAnchor = GetAnchor((Canis::RectAnchor)rect_transform.anchor,
+                                        (float)window->GetScreenWidth(),
+                                        (float)window->GetScreenHeight());
+
+            mouse = GetInputManager().mouse;
+
+            if (rect_transform.rotation != 0.0f)
+            {
+                float cAngle = cos(-rect_transform.rotation);
+                float sAngle = sin(-rect_transform.rotation);
+
+                glm::vec2 mouseOffset = mouse - (rect_transform.position + rect_transform.originOffset + positionAnchor);
+
+                RotatePoint(mouseOffset, cAngle, sAngle);
+
+                mouse = mouseOffset + (rect_transform.position + rect_transform.originOffset + positionAnchor);
+            }
+
+            if (mouse.x > rect_transform.position.x + positionAnchor.x + rect_transform.originOffset.x &&
+                mouse.x < rect_transform.position.x + (rect_transform.size.x * rect_transform.scale) + rect_transform.originOffset.x + positionAnchor.x &&
+                mouse.y > rect_transform.position.y + positionAnchor.y + rect_transform.originOffset.y &&
+                mouse.y < rect_transform.position.y + (rect_transform.size.y * rect_transform.scale) + rect_transform.originOffset.y + positionAnchor.y)
+            {
+                color.color = button.hoverColor;
+                button.mouseOver = true;
+                rect_transform.scale = button.hoverScale;
+
+                if (button.action == 0u)
+                {
+                    if (inputManager->JustLeftClicked())
+                    {
+                        button.func(button.instance);
+                    }
+                }
+                else if (button.action == 1u)
+                {
+                    if (inputManager->LeftClickReleased())
+                    {
+                        button.func(button.instance);
+                    }
+                }
+                return;                    
+            }            
         }
     }
 
