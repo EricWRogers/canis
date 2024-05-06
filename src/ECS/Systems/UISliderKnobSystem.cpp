2d9833e6f363e12d48eade1fc91dc8f96752d47f
EricWRogers <ericwilliamrogers@gmail.com>
2024-05-06 15:34:47 -0500

    Only call slider on changed function when a change happens

--- src/ECS/Systems/UISliderKnobSystem.cpp
+++ src/ECS/Systems/UISliderKnobSystem.cpp
@@ -14,26 +14,25 @@ namespace Canis
     void UISliderKnobSystem::Update(entt::registry &_registry, float _deltaTime)
     {
         bool mouseDown = GetInputManager().GetLeftClick();
-        
+
         auto view = _registry.view<RectTransformComponent, ButtonComponent, UISliderKnobComponent>();
         for (auto [entity, rect, button, knob] : view.each())
         {
             if (!knob.slider)
                 return;
-            
+
             if (mouseDown == false)
                 knob.grabbed = false;
 
-            UISliderComponent& slider = knob.slider.GetComponent<UISliderComponent>();
-            RectTransformComponent& sliderRect = knob.slider.GetComponent<RectTransformComponent>();
+            UISliderComponent &slider = knob.slider.GetComponent<UISliderComponent>();
+            RectTransformComponent &sliderRect = knob.slider.GetComponent<RectTransformComponent>();
 
-            vec2 halfSize {
+            vec2 halfSize{
                 (rect.size.x * rect.scale) / 2.0f,
-                (rect.size.y * rect.scale) / 2.0f
-            };
-            
+                (rect.size.y * rect.scale) / 2.0f};
+
             SetUISliderTarget(slider, knob.value, 0.0f);
-            
+
             rect.anchor = sliderRect.anchor;
             rect.position.x = sliderRect.position.x + (knob.value * slider.maxWidth) - halfSize.x;
             rect.position.y = (sliderRect.position.y + (sliderRect.size.y * sliderRect.scale) / 2.0f) - halfSize.y;
@@ -48,16 +47,22 @@ namespace Canis
 
             rect.position.x = GetInputManager().mouse.x - (rect.size.x * rect.scale) / 2.0f;
             rect.position.x -= GetAnchor(
-                (RectAnchor)rect.anchor,
-                GetWindow().GetScreenWidth(),
-                GetWindow().GetScreenHeight()).x;
-            
+                                   (RectAnchor)rect.anchor,
+                                   GetWindow().GetScreenWidth(),
+                                   GetWindow().GetScreenHeight())
+                                   .x;
+
             Clamp(rect.position.x, sliderRect.position.x - halfSize.x, (sliderRect.position.x + slider.maxWidth) - halfSize.x);
-            knob.value = (rect.position.x - sliderRect.position.x + halfSize.x) / slider.maxWidth;
-            
-            if (knob.OnChanged)
+            float newValue = (rect.position.x - sliderRect.position.x + halfSize.x) / slider.maxWidth;
+
+            if (knob.value != newValue)
             {
-                knob.OnChanged(knob.value);
+                knob.value = newValue;
+
+                if (knob.OnChanged)
+                {
+                    knob.OnChanged(knob.value);
+                }
             }
         }
     }
