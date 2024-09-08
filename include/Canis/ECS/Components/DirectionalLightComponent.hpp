ae26686fd05b3b7b4aed6ddf6b96b7633bc085b6
EricWRogers <ericwilliamrogers@gmail.com>
2024-09-07 21:14:59 -0500

    Automate more components

--- include/Canis/ECS/Components/DirectionalLightComponent.hpp
+++ include/Canis/ECS/Components/DirectionalLightComponent.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <glm/glm.hpp>
+#include <Canis/Yaml.hpp>
 
 namespace Canis
 {
@@ -10,5 +11,13 @@ namespace Canis
         glm::vec3 ambient = glm::vec3(0.05f, 0.05f, 0.05f);
         glm::vec3 diffuse = glm::vec3(0.8f, 0.8f, 0.8f);
         glm::vec3 specular = glm::vec3(0.5f, 0.5f, 0.5f);
+
+        static void RegisterProperties()
+		{
+			REGISTER_PROPERTY(DirectionalLightComponent, direction, glm::vec3);
+			REGISTER_PROPERTY(DirectionalLightComponent,   ambient, glm::vec3);
+			REGISTER_PROPERTY(DirectionalLightComponent,   diffuse, glm::vec3);
+			REGISTER_PROPERTY(DirectionalLightComponent,  specular, glm::vec3);
+		}
 	};
 } // end of Canis namespace
