67b6d04357b214ab0c41677cc3a82d87db3fc187
EricWRogers <ericwilliamrogers@gmail.com>
2025-06-08 07:39:13 -0500

    Animation Helper Function

--- include/Canis/ECS/Components/SpriteAnimationComponent.hpp
+++ include/Canis/ECS/Components/SpriteAnimationComponent.hpp
@@ -11,5 +11,18 @@ namespace Canis
         bool flipY = false;
         bool redraw = true;
         float speed = 1.0f;
+
+        void Play(std::string _path)
+        {
+            speed = 1.0f;
+            animationId = Canis::AssetManager::LoadSpriteAnimation(_path);
+            index = 0;
+            redraw = true;
+        }
+
+        void Pause()
+        {
+            speed = 0.0f;
+        }
     };
 } // end of Canis namespace
